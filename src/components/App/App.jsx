c983175b9e94a6b7cb88e64b644483507bb915ca
OlegOliinyk <91748610+OlegOliinyk@users.noreply.github.com>
2025-04-13 16:46:07 +0300

    Correction

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,21 @@
-import { Suspense, lazy, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { Toaster } from "react-hot-toast";
-import { refreshUser } from "../../redux/auth/operations";
-import { selectIsRefreshing } from "../../redux/auth/selectors";
-import { RestrictedRoute } from "../RestrictedRoute";
-import { PrivateRoute } from "../PrivateRoute";
-import Layout from "../Layout/Layout";
+import { Suspense, lazy, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+import { refreshUser } from '../../redux/auth/operations';
+import { selectIsRefreshing } from '../../redux/auth/selectors';
+import { RestrictedRoute } from '../RestrictedRoute';
+import { PrivateRoute } from '../PrivateRoute';
+import Layout from '../Layout/Layout';
+import './App.css';
 
-const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
 const RegistrationPage = lazy(() =>
-  import("../../pages/RegistrationPage/RegistrationPage")
+  import('../../pages/RegistrationPage/RegistrationPage')
 );
-const LoginPage = lazy(() => import("../../pages/LoginPage/LoginPage"));
+const LoginPage = lazy(() => import('../../pages/LoginPage/LoginPage'));
 const ContactsPage = lazy(() =>
-  import("../../pages/ContactsPage/ContactsPage")
+  import('../../pages/ContactsPage/ContactsPage')
 );
 
 const App = () => {
--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -24,13 +24,14 @@
 
 // export default App;
 
-import ContactForm from "../../components/ContactForm/ContactForm";
-import SearchBox from "../../components/SearchBox/SearchBox";
-import ContactList from "../../components/ContactList/ContactList";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fetchContacts } from "../../redux/contacts/operations";
-import toast from "react-hot-toast";
+import ContactForm from '../../components/ContactForm/ContactForm';
+import SearchBox from '../../components/SearchBox/SearchBox';
+import ContactList from '../../components/ContactList/ContactList';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from '../../redux/contacts/operations';
+import toast from 'react-hot-toast';
+import styles from './ContactsPage.module.css';
 
 const ContactsPage = () => {
   const dispatch = useDispatch();
@@ -39,13 +40,13 @@ const ContactsPage = () => {
     dispatch(fetchContacts())
       .unwrap()
       .catch((e) => {
-        toast.error("Sorry, we were unable to access your contact list..");
+        toast.error('Sorry, we were unable to access your contact list..');
         console.log(e);
       });
   }, [dispatch]);
 
   return (
-    <div className="">
+    <div className={styles.contactsPage}>
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
